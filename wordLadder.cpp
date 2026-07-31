393c91fa066039d28dac787a2c08b174573fd518
Saily-Kadam <kadamsaily761@gmail.com>
2026-07-31 09:45:29 +0530

    Add files via upload

---
+++ wordLadder.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        if(!dict.count(endWord))
+            return 0;
+        queue<string> q;
+        q.push(beginWord);
+
+        dict.erase(beginWord);
+        int level = 1;
+
+        while(!q.empty()) {
+            int sz = q.size();
+            while(sz--) {
+                string word = q.front();
+                q.pop();
+
+                if(word == endWord)
+                    return level;
+
+                for(int i = 0; i < word.size(); i++) {
+
+                    char original = word[i];
+
+                    for(char ch = 'a'; ch <= 'z'; ch++) {
+
+                        if(ch == original)
+                            continue;
+
+                        word[i] = ch;
+
+                        if(dict.count(word)) {
+                            q.push(word);
+                            dict.erase(word);
+                        }
+                    }
+                    word[i] = original;
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+};
